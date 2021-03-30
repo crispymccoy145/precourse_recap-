a0641836b8659b3681da12f01f90bda610c0ea2d
stmccoy <76181026+stmccoy@users.noreply.github.com>
2021-03-30 10:00:25 +0100

    Update precourse_recap.py

--- precourse_recap.py
+++ precourse_recap.py
@@ -9,10 +9,10 @@ print("Guess the number between 1 and 100")
 #creates a loop of guessing that only ends when the client guesses correctly
 while True:
     y = int(input('What is your guess? '))
-    if y < x:
-        print("too low")
-    if x < y:
-        print("too high")
     if x == y:
         print("correct answer")
         break
+    elif y < x:
+        print("too low")
+    else:
+        print("too high")
